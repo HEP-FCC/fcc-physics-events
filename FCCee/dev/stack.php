d59f1a42356da552ea5b05bb6985ab24b63d165d
Juraj Smiesko <juraj.smiesko@cern.ch>
2024-02-20 20:40:06 +0000

    Removing stack.php and topbar.php

--- FCCee/dev/stack.php
+++
@@ -1,5 +0,0 @@
-      <p class="mt-5">
-	<a href="https://cern.ch/key4hep/">Key4hep</a> stack used during the generation of the
-        <code>dev</code> samples was:
-        <pre><code>/cvmfs/sw.hsf.org/key4hep/setup.sh</code></pre>
-      </p>
